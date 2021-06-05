c7cb843c9214cba78e8d3a8e71febb957aaeaeb4
savosburn <savosburn18@tampabay.rr.com>
2021-06-04 20:08:11 -0400

    Initial commit

---
+++ src/main/java/ex15/App.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
+package ex15;
+
+/*
+Passwords are validated by comparing a user-provided value with a known value that’s stored. Either it’s correct or it’s not.
+
+Create a simple program that validates user login credentials. The program must prompt the user for a username and password. The program should compare the password given by the user to a known password. If the password matches, the program should display “Welcome!” If it doesn’t match, the program should display “I don’t know you.”
+
+Example Output
+What is the password? 12345
+I don't know you.
+Or
+
+What is the password? abc$123
+Welcome!
+Constraints
+Use an if/else statement for this problem.
+Make sure the program is case sensitive.
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
