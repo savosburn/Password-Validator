7e274aea8eaabe8da61be2fd3b950582a9a695ea
savosburn <savosburn18@tampabay.rr.com>
2021-06-06 17:29:08 -0400

    update test cases

--- src/test/java/ex15/AppTest.java
+++ src/test/java/ex15/AppTest.java
@@ -11,17 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
-    public void output_string_two_options () {
+    public void output_same_password_wrong_case () {
         App myApp = new App();
 
         String password = "ABC$123";
-        String expectedOutput;
+        String expectedOutput = "I don't know you.";
 
-        if (password.equals("abc$123")) {
-            expectedOutput = "Welcome!";
-        } else {
-            expectedOutput = "I don't know you." ;
-        }
+        String actualOutput = myApp.generateOutput(password);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void output_different_password () {
+        App myApp = new App();
+
+        String password = "heLLO56";
+        String expectedOutput = "I don't know you.";
+
+        String actualOutput = myApp.generateOutput(password);
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void output_same_password () {
+        App myApp = new App();
+
+        String password = "abc$123";
+        String expectedOutput = "Welcome!";
 
         String actualOutput = myApp.generateOutput(password);
 
